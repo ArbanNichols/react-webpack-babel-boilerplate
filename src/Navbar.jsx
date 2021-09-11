5e2952604576717953d44f3acf6ce7aaf6032316
code_shoulder <arban.nichs@gmail.com>
2021-09-11 11:37:21 -0400

    Added Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -9,9 +9,22 @@ const Navbar = () => {
       <div className='nav-center'>
         <div className='nav-header'>
           <img src={logo} alt='stripe' className='nav-logo' />
-          <button className='btn toggle-btn' onClick={openSidebar}></button>
+          <button className='btn toggle-btn' onClick={openSidebar}>
+            <FaBars />
+          </button>
         </div>
-        <ul className='navlinks'></ul>
+        <ul className='nav-links'>
+          <li>
+            <button className='link-btn'>products</button>
+          </li>
+          <li>
+            <button className='link-btn'>developers</button>
+          </li>
+          <li>
+            <button className='link-btn'>company</button>
+          </li>
+        </ul>
+        <button className='btn signin-btn'>Sign In</button>
       </div>
     </nav>
   );
